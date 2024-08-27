f50f198c5b4ed3f68045a2cbea911555085b2cd1
Jose Javier Columbie <josejaviercolumbie@yahoo.com>
2024-08-26 17:15:34 -0700

    Add project files.

---
+++ XAFTemplateListViewImage.Blazor.Server/Controllers/TestViewController1.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XAFTemplateListViewImage.Module.BusinessObjects;
+
+namespace XAFTemplateListViewImage.Blazor.Server.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class TestViewController1 : ViewController
+    {
+        SimpleAction action;
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public TestViewController1()
+        {
+            InitializeComponent();
+            action = new SimpleAction(this, "Open Popup", "View");
+            action.Execute += action_Execute;
+            
+
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        private void action_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            Application.ShowViewStrategy.ShowViewInPopupWindow(Application.CreateListView(typeof(Birthday), false));
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
---
+++ XAFTemplateListViewImage.Blazor.Server/Templates/PopupWindowTemplate1.cs
@@ -0,0 +1,71 @@
+﻿using DevExpress.Blazor;
+using DevExpress.ExpressApp.Blazor.Components.Models;
+using DevExpress.ExpressApp.Blazor.Editors.ActionControls;
+using DevExpress.ExpressApp.Blazor.Templates;
+using DevExpress.ExpressApp.Blazor.Templates.ContextMenu.ActionControls;
+using DevExpress.ExpressApp.Blazor.Templates.Toolbar.ActionControls;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Templates.ActionControls;
+using DevExpress.Persistent.Base;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XAFTemplateListViewImage.Blazor.Server.Templates
+{
+    public class PopupWindowTemplate1 : PopupWindowTemplateBase, ISupportActionsToolbarVisibility, ISupportListEditorInlineActions, ISupportListEditorContextMenuActions, ITemplateToolbarProvider
+    {
+        public PopupWindowTemplate1() : base()
+        {
+            Popup.MinHeight = "200px";
+            Popup.MinWidth = "300px";
+            Popup.Width = "min(800px, 100%)";
+            Popup.MaxHeight = "calc(100vh - var(--is-widescreen) * 3.5rem)";
+
+            IsActionsToolbarVisible = true;
+            Toolbar = new DxToolbarAdapter(new DxToolbarModel());
+            Toolbar.AddActionContainer(nameof(PredefinedCategory.ObjectsCreation));
+            Toolbar.AddActionContainer(nameof(PredefinedCategory.Search), ToolbarItemAlignment.Right);
+            Toolbar.AddActionContainer(nameof(PredefinedCategory.Filters), ToolbarItemAlignment.Right);
+            Toolbar.AddActionContainer(nameof(PredefinedCategory.FullTextSearch), ToolbarItemAlignment.Right);
+
+            BottomToolbar = new DxToolbarAdapter(new DxToolbarModel());
+            BottomToolbar.AddActionContainer(nameof(PredefinedCategory.Diagnostic), ToolbarItemAlignment.Right);
+            BottomToolbar.AddActionContainer(DialogController.DialogActionContainerName, ToolbarItemAlignment.Right);
+            BottomToolbar.ToolbarModel.SizeMode = SizeMode.Large;
+
+            ListEditorActionColumnAdapter = new ListEditorActionColumnAdapter();
+            ListEditorActionColumnAdapter.AddActionContainer(nameof(PredefinedCategory.ListView));
+            ListEditorActionColumnAdapter.AddActionContainer(nameof(PredefinedCategory.Edit));
+            ListEditorActionColumnAdapter.AddActionContainer(nameof(PredefinedCategory.RecordEdit));
+
+            DxContextMenuAdapter = new DxContextMenuAdapter(new DxContextMenuModel());
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.ObjectsCreation));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.Save));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.SaveOptions), isDropDown: true, defaultActionId: "SaveAndNew", autoChangeDefaultAction: true);
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.Edit));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.RecordEdit));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.UndoRedo));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.Print));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.View));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.Export));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.Reports));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.OpenObject));
+            DxContextMenuAdapter.AddActionContainer(nameof(PredefinedCategory.Menu));
+        }
+        protected override IEnumerable<IActionControlContainer> GetActionControlContainers() =>
+            Toolbar.ActionContainers.Concat(BottomToolbar.ActionContainers.Concat(ListEditorActionColumnAdapter.ActionContainers.Union(DxContextMenuAdapter.ActionContainers)));
+        protected override RenderFragment CreateComponent() => PopupWindowTemplate1Component.Create(this);
+
+        public bool IsActionsToolbarVisible { get; private set; }
+        public DxToolbarAdapter Toolbar { get; }
+        public DxToolbarAdapter BottomToolbar { get; }
+        public ListEditorActionColumnAdapter ListEditorActionColumnAdapter { get; }
+        public DxContextMenuAdapter DxContextMenuAdapter { get; }
+
+        #region ISupportActionsToolbarVisibility
+        void ISupportActionsToolbarVisibility.SetVisible(bool isVisible) => IsActionsToolbarVisible = isVisible;
+        #endregion
+    }
+}
